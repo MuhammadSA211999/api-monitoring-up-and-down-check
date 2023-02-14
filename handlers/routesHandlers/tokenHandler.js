5ac2f91edeb0e7733d02c9fe8468962dc5c44a3e
Muhammad SA <MuhammadSA211999>
2023-02-15 00:44:11 +0600

    envalid property left-side error fixing

--- handlers/routesHandlers/tokenHandler.js
+++ handlers/routesHandlers/tokenHandler.js
@@ -1,5 +1,5 @@
 // dependecies
-const { hash, parseJSON } = require("../../helpers/utilities")
+const { hash, parseJSON, createARandomCharacter } = require("../../helpers/utilities")
 const data = require("../../lib/data")
 
 const handler = {}
@@ -25,6 +25,32 @@ handlers.tokens.get = (requestedProperties, callback) = {
 handler.tokens.post = (requestedProperties, callback) = {
     const phone = typeof requestedProperties.body.phone === 'string' && requestedProperties.body.phone.trim().length === 20 ? requestedProperties.body.phone : false
     const password = typeof requestedProperties.body.password === 'string' && requestedProperties.body.password.trim().length > 0 ? requestedProperties.body.password : false
+    if(phone&& password) {
+    data.read('users', phone, (err1, userData) => {
+        if (!err1 && userData) {
+            const user = { ...parseJSON(userData) }
+            const hashPass = hash(password)
+            if (user.password === hashPass) {
+                const tokenId = createARandomCharacter(20)
+                const expires = Date.now() + 60 * 60 * 1000
+                const tokenObj = {
+                    hashPass, tokenId, expires
+                }
+
+            }
+        }
+        else {
+            callback(404, {
+                error: 'problem to yor request'
+            })
+        }
+    })
+}
+    else {
+    callback(404, {
+        error: 'problem to yor request'
+    })
+}
 };
 
 handlers.tokens.put = (requestedProperties, callback) = {
