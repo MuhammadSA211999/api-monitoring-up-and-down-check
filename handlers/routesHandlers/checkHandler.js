08707740de5dd6e19475d838e282cdbb227ad969
Muhammad SA <MuhammadSA211999>
2023-02-17 20:44:32 +0600

    successfully done create a check

--- handlers/routesHandlers/checkHandler.js
+++ handlers/routesHandlers/checkHandler.js
@@ -1,5 +1,5 @@
 // dependecies
-const { hash, parseJSON } = require("../../helpers/utilities")
+const { hash, parseJSON, createARandomCharacter } = require("../../helpers/utilities")
 const data = require("../../lib/data")
 const toke = require('./tokenHandler')
 const handler = {}
@@ -30,7 +30,87 @@ handlers._check.post = (requestedProperties, callback) = {
     const timeoutSec = typeof (requestedProperties.body.time) === 'number' && requestedProperties.body.time >= 1 && requestedProperties.body.time <= 5 && requestedProperties.body.time % 1 === 0 ? requestedProperties.body.time : false
 
     if(protocol&& url && method && successCode && timeoutSec) {
+
     const tokenId = typeof (requestedProperties.headersObj.token) === 'string' && requestedProperties.headersObj.token.length === 20 ? requestedProperties.headersObj.token : false
+
+    //lookup the token for verification
+    data.read('tokens', tokenId, (err1, tokenData) => {
+        if (!err1 && tokenData) {
+            const tokenObj = { ...parseJSON(tokenData) }
+            const phone = tokenObj.phone
+            // lookup the users
+            data.read('users', phone, (err2, userData) => {
+                if (!err2 && userData) {
+                    const user = { ...parseJSON(userData) }
+                    tokenHandler.token._verify(tokenId, phone, (isValid) => {
+                        if (isValid) {
+                            const checks = typeof user.checks === 'object' && user.checks instanceof Array ? user.checks : []
+                            if (checks.length < 5) {
+                                const checkObj = {
+                                    checkId = createARandomCharacter(20),
+                                    phone: phone,
+                                    protocol: protocol,
+                                    method: method,
+                                    url: url,
+                                    successCode: successCode,
+                                    timeoutSec
+                                }
+                                data.create('checks', checkId, checkObj, (err2) => {
+                                    if (!err2) {
+                                        userObj.checks.push(checkId)
+                                        // update the user file
+                                        data.update('users', phone, userObj, (err3) => {
+                                            if (!err3) {
+                                                callback(200, {
+                                                    message: 'successfully update the the users'
+                                                })
+                                            }
+                                            else {
+                                                callback(500, {
+                                                    error: 'something went wrong'
+                                                })
+                                            }
+                                    else {
+                                                callback(500, {
+                                                    error: 'something went wrong'
+                                                })
+                                            }
+                                        })
+                                    }
+                                    else {
+                                        callback(401, {
+                                            error: 'could not create the check'
+                                        })
+                                    }
+                                })
+                            }
+                            else {
+                                callback(401, {
+                                    error: 'You has already reached the checks limit'
+                                })
+                            }
+
+                        }
+                        else {
+                            callback(500, {
+                                error: 'token is not valid'
+                            })
+                        }
+                    })
+                }
+                else {
+                    callback(500, {
+                        error: 'server side problem'
+                    })
+                }
+            })
+        }
+        else {
+            callback(403, {
+                error: 'authentication failed'
+            })
+        }
+    })
 }
     else {
     callback(404, {
