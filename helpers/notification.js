bd2bbf8c9b0fea0f6a4523a5e324eec8154c2d11
Muhammad SA <MuhammadSA211999>
2023-02-17 22:59:38 +0600

    twilio sms boilering setup

--- helpers/notification.js
+++ helpers/notification.js
@@ -1,7 +1,14 @@
 const https = require('https')
 const notification = {}
-notification.twilioSMS = (phone, msg, callback) => {
+notification.sendTwilioSMS = (phone, msg, callback) => {
+    const msg = typeof msg === 'string' && msg.trim().length > 0 && msg.trim().length <= 1600 ? msg : false
+    const phone = typeof phone === 'string' && phone.trim().length === '11' ? msg : false
+    if (msg && phone) {
 
+    }
+    else {
+        callback('Something missing in your request')
+    }
 }
 
 module.exports = notification
