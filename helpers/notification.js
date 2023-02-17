9f0f97951135469f18ffe3e1362e40364ed28bec
Muhammad SA <MuhammadSA211999>
2023-02-17 23:08:40 +0600

    twilio config

--- helpers/notification.js
+++ helpers/notification.js
@@ -1,10 +1,25 @@
 const https = require('https')
+const querystring = require('querystring')
 const notification = {}
 notification.sendTwilioSMS = (phone, msg, callback) => {
     const msg = typeof msg === 'string' && msg.trim().length > 0 && msg.trim().length <= 1600 ? msg : false
     const phone = typeof phone === 'string' && phone.trim().length === '11' ? msg : false
     if (msg && phone) {
-
+        const payload = {
+            from: '+8801322697553',
+            to: phone,
+            body: msg
+        }
+        const payloadString = querystring(payload)
+        const requestDetails = {
+            hostname: 'api.twilio.com',
+            method: 'POST',
+            path: `${twilio.accountSid}`,
+            auth: `${twilio.authToken}`,
+            headers: {
+                'Content-Type': 'x-www-form-urlencoded'
+            }
+        }
     }
     else {
         callback('Something missing in your request')
